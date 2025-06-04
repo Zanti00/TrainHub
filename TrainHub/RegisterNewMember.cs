b158b637719c62c0b55db4671edb7948a204a153
Micaella Eliab <micaellageliab@iskolarngbayan.pup.edu.ph>
2025-06-04 18:11:17 +0800

    A Component to Add/ Register a New Member

---
+++ TrainHub/RegisterNewMember.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrainHub
+{
+    public partial class RegisterNewMember : Form
+    {
+        public RegisterNewMember()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cuiTextBox1_ContentChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void loginBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cuiComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cuiComboBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
