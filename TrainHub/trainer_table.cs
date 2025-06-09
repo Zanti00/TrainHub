b8065e5cdace93a47b166f5252a9e2d11b4fc6d5
angelicapanergo <angelicajoyppanergo@iskolarngbayan.pup.edu.ph>
2025-06-09 17:34:08 +0800

    fixing

--- TrainHub/trainer_table.cs
+++ TrainHub/trainer_table.cs
@@ -19,7 +19,11 @@ namespace TrainHub
 
         private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-
+            if (e.ColumnIndex == 10 && e.RowIndex >= 0)
+            {
+                edit_trainer editForm = new edit_trainer();
+                editForm.ShowDialog();
+            }
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -39,7 +43,22 @@ namespace TrainHub
 
         private void cuiButtonGroup3_Click(object sender, EventArgs e)
         {
-
+            if (dataGridView2.SelectedRows.Count > 0)
+            {
+                DialogResult result = MessageBox.Show("Are you sure you want to delete this trainer?",
+                                                    "Confirm Delete",
+                                                    MessageBoxButtons.YesNo,
+                                                    MessageBoxIcon.Question);
+                if (result == DialogResult.Yes)
+                {
+                    // Delete logic here
+                    dataGridView2.Rows.RemoveAt(dataGridView2.SelectedRows[0].Index);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a trainer to delete.", "No Selection");
+            }
         }
 
         private void cuiTextBox1_ContentChanged(object sender, EventArgs e)
