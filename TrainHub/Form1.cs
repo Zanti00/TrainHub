de8a051b9f51a3096f8dc7e7746545ef158bdd57
angelicapanergo <angelicajoyppanergo@iskolarngbayan.pup.edu.ph>
2025-06-14 13:57:07 +0800

    updated table

---
+++ TrainHub/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrainHub
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void advancedDataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void cuiButtonGroup2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void searchBar_ContentChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
