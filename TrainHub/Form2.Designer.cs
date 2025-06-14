fc4f5d3d14ecbac66015d760d724d730e176c1d2
angelicapanergo <angelicajoyppanergo@iskolarngbayan.pup.edu.ph>
2025-06-14 16:13:32 +0800

    added grid view

--- TrainHub/Form2.Designer.cs
+++ TrainHub/Form2.Designer.cs
@@ -30,10 +30,14 @@
         {
             tableLayoutPanel1 = new TableLayoutPanel();
             panel1 = new Panel();
-            label1 = new Label();
             label2 = new Label();
+            label1 = new Label();
+            cuiPanel1 = new CuoreUI.Controls.cuiPanel();
+            cuiButton1 = new CuoreUI.Controls.cuiButton();
+            cuiPanel2 = new CuoreUI.Controls.cuiPanel();
             tableLayoutPanel1.SuspendLayout();
             panel1.SuspendLayout();
+            cuiPanel1.SuspendLayout();
             SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -41,11 +45,14 @@
             tableLayoutPanel1.ColumnCount = 1;
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
             tableLayoutPanel1.Controls.Add(panel1, 0, 0);
+            tableLayoutPanel1.Controls.Add(cuiPanel1, 0, 1);
+            tableLayoutPanel1.Controls.Add(cuiPanel2, 0, 2);
             tableLayoutPanel1.Location = new Point(-1, 1);
             tableLayoutPanel1.Name = "tableLayoutPanel1";
-            tableLayoutPanel1.RowCount = 2;
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 11.9205294F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 88.07947F));
+            tableLayoutPanel1.RowCount = 3;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 47.8632469F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 52.1367531F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 352F));
             tableLayoutPanel1.Size = new Size(800, 453);
             tableLayoutPanel1.TabIndex = 0;
             // 
@@ -55,8 +62,19 @@
             panel1.Controls.Add(label1);
             panel1.Location = new Point(3, 3);
             panel1.Name = "panel1";
-            panel1.Size = new Size(200, 48);
+            panel1.Size = new Size(200, 42);
             panel1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = SystemColors.Control;
+            label2.ForeColor = Color.Gray;
+            label2.Location = new Point(3, 29);
+            label2.Name = "label2";
+            label2.Size = new Size(144, 15);
+            label2.TabIndex = 26;
+            label2.Text = "View and Manage Trainers";
             // 
             // label1
             // 
@@ -70,16 +88,66 @@
             label1.TabIndex = 25;
             label1.Text = "Trainers\r\n";
             // 
-            // label2
+            // cuiPanel1
             // 
-            label2.AutoSize = true;
-            label2.BackColor = SystemColors.Control;
-            label2.ForeColor = Color.Gray;
-            label2.Location = new Point(3, 29);
-            label2.Name = "label2";
-            label2.Size = new Size(144, 15);
-            label2.TabIndex = 26;
-            label2.Text = "View and Manage Trainers";
+            cuiPanel1.Controls.Add(cuiButton1);
+            cuiPanel1.Dock = DockStyle.Right;
+            cuiPanel1.Location = new Point(605, 51);
+            cuiPanel1.Name = "cuiPanel1";
+            cuiPanel1.OutlineThickness = 1F;
+            cuiPanel1.PanelColor = SystemColors.Control;
+            cuiPanel1.PanelOutlineColor = SystemColors.Control;
+            cuiPanel1.Rounding = new Padding(8);
+            cuiPanel1.Size = new Size(192, 46);
+            cuiPanel1.TabIndex = 1;
+            // 
+            // cuiButton1
+            // 
+            cuiButton1.CheckButton = false;
+            cuiButton1.Checked = false;
+            cuiButton1.CheckedBackground = Color.FromArgb(255, 106, 0);
+            cuiButton1.CheckedForeColor = Color.White;
+            cuiButton1.CheckedImageTint = Color.White;
+            cuiButton1.CheckedOutline = Color.FromArgb(255, 106, 0);
+            cuiButton1.Content = "Your text here!";
+            cuiButton1.DialogResult = DialogResult.None;
+            cuiButton1.Font = new Font("Microsoft Sans Serif", 9.75F);
+            cuiButton1.ForeColor = Color.WhiteSmoke;
+            cuiButton1.HoverBackground = Color.FromArgb(51, 81, 88);
+            cuiButton1.HoveredImageTint = Color.White;
+            cuiButton1.HoverForeColor = Color.Black;
+            cuiButton1.HoverOutline = Color.FromArgb(32, 128, 128, 128);
+            cuiButton1.Image = null;
+            cuiButton1.ImageAutoCenter = true;
+            cuiButton1.ImageExpand = new Point(0, 0);
+            cuiButton1.ImageOffset = new Point(0, 0);
+            cuiButton1.Location = new Point(26, 1);
+            cuiButton1.Name = "cuiButton1";
+            cuiButton1.NormalBackground = Color.FromArgb(51, 81, 88);
+            cuiButton1.NormalForeColor = Color.WhiteSmoke;
+            cuiButton1.NormalImageTint = Color.White;
+            cuiButton1.NormalOutline = Color.FromArgb(64, 128, 128, 128);
+            cuiButton1.OutlineThickness = 1F;
+            cuiButton1.PressedBackground = Color.WhiteSmoke;
+            cuiButton1.PressedForeColor = Color.FromArgb(32, 32, 32);
+            cuiButton1.PressedImageTint = Color.White;
+            cuiButton1.PressedOutline = Color.FromArgb(64, 128, 128, 128);
+            cuiButton1.Rounding = new Padding(8);
+            cuiButton1.Size = new Size(148, 45);
+            cuiButton1.TabIndex = 0;
+            cuiButton1.TextAlignment = StringAlignment.Center;
+            cuiButton1.TextOffset = new Point(0, 0);
+            // 
+            // cuiPanel2
+            // 
+            cuiPanel2.Location = new Point(3, 103);
+            cuiPanel2.Name = "cuiPanel2";
+            cuiPanel2.OutlineThickness = 1F;
+            cuiPanel2.PanelColor = Color.FromArgb(255, 106, 0);
+            cuiPanel2.PanelOutlineColor = Color.FromArgb(255, 106, 0);
+            cuiPanel2.Rounding = new Padding(8);
+            cuiPanel2.Size = new Size(794, 58);
+            cuiPanel2.TabIndex = 2;
             // 
             // Form2
             // 
@@ -92,6 +160,7 @@
             tableLayoutPanel1.ResumeLayout(false);
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
+            cuiPanel1.ResumeLayout(false);
             ResumeLayout(false);
         }
 
@@ -101,5 +170,8 @@
         private Panel panel1;
         private Label label1;
         private Label label2;
+        private CuoreUI.Controls.cuiPanel cuiPanel1;
+        private CuoreUI.Controls.cuiButton cuiButton1;
+        private CuoreUI.Controls.cuiPanel cuiPanel2;
     }
 }
